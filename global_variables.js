adc7ce808a99405f16ad672b0e73dc1a44a86baa
quasario <tamirlan.attaev@axxonsoft.dev>
2023-06-13 21:48:00 +0300

    Removed unused files

--- global_variables.js
+++ global_variables.js
@@ -1,7 +1,7 @@
 import { getHostName } from "./http_api/http_host";
 import { green, blue, yellow, red } from 'colors'
 
-export let currentURL = "http://127.0.0.1"; //"http://192.168.0.24"
+export let currentURL = "http://192.168.0.24"; //"http://127.0.0.1"
 export let isLocalMachine = false; //если тесты и сервер находятся на одной машине - true
 export let hostName;
 export let videoFolder = 'C:/demovideo';
